97ee489fb38f2c2e0ef297979fee5875fe36f38c
lucas castro <lucas_o__castro@hotmail.com>
2024-12-07 19:39:48 -0300

    Adds concrete implementation of BaseStateMachine which accepts any IState and ITransition

---
+++ Assets/_Project/Scripts/Shared/StateMachines/FuncPredicate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Shared.StateMachines
+{
+    public class FuncPredicate : IPredicate
+    {
+        private readonly Func<bool> _predicate;
+        
+        public FuncPredicate(Func<bool> predicate)
+        {
+            _predicate = predicate;
+        }
+
+        public bool Evaluate() => _predicate();
+        
+        public static implicit operator FuncPredicate(Func<bool> predicate) => new(predicate);
+    }
+}
---
+++ Assets/_Project/Scripts/Shared/StateMachines/Transition.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.StateMachines
+{
+    public class Transition : BaseTransition<IState>
+    {
+        public Transition(IState to, IPredicate condition) : base(to, condition) { }
+    }
+}
