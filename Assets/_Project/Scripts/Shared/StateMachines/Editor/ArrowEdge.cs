000482e82c0432bbfb8b7d2a3b8a8508ebabcf4f
lucas castro <lucas_o__castro@hotmail.com>
2024-12-16 22:05:49 -0300

    Draw arrow on edge end

--- Assets/_Project/Scripts/Shared/StateMachines/Editor/ArrowEdge.cs
+++ Assets/_Project/Scripts/Shared/StateMachines/Editor/ArrowEdge.cs
@@ -8,6 +8,9 @@ namespace Shared.StateMachines.Editor
 {
     public class ArrowEdge : Edge
     {
+        public float ArrowHeight = 15f;
+        public float ArrowBase = 10f;
+        
         public StateMachineGraphView GraphView
         {
             get
@@ -23,21 +26,51 @@ namespace Shared.StateMachines.Editor
         public ArrowEdge()
         {
             edgeControl.RegisterCallback<GeometryChangedEvent>(OnGeometryChanged);
+            edgeControl.generateVisualContent += DrawArrow;
         }
 
         private void OnGeometryChanged(GeometryChangedEvent evt)
         {
             //Offset to node edge
             Vector2 dir = (PointsAndTangents[3] - PointsAndTangents[0]).normalized;
-            PointsAndTangents[0] = Offset(PointsAndTangents[0], dir);
-            PointsAndTangents[3] = Offset(PointsAndTangents[3], -dir);
+            PointsAndTangents[0] = OffsetToEdge(PointsAndTangents[0], dir);
+            PointsAndTangents[3] = OffsetToEdge(PointsAndTangents[3], -dir);
+            
+            //Offset for arrow
+            PointsAndTangents[3] -= dir * ArrowHeight;
             
             // Make straight
             PointsAndTangents[1] = PointsAndTangents[0];
             PointsAndTangents[2] = PointsAndTangents[3];
+            
+            // Disable the round caps
+            edgeControl.drawToCap = edgeControl.drawFromCap = false;
         }
 
-        private Vector2 Offset(Vector2 localPos, Vector2 dir)
+        private void DrawArrow(MeshGenerationContext meshGenerationContext)
+        {
+            var painter = meshGenerationContext.painter2D;
+
+            painter.strokeColor = painter.fillColor = edgeControl.toCapColor;
+            painter.lineWidth = edgeControl.edgeWidth;
+
+            Vector2 basePoint = edgeControl.parent.ChangeCoordinatesTo(edgeControl, PointsAndTangents[3]); 
+            Vector2 dir = (PointsAndTangents[3] - PointsAndTangents[0]).normalized;
+            Vector2 tip = basePoint + dir * ArrowHeight;
+
+            Vector2 baseDir = Vector2.Perpendicular(dir);
+            Vector2 a = basePoint + baseDir * ArrowBase * .5f;
+            Vector2 b = basePoint - baseDir * ArrowBase * .5f;
+            
+            painter.BeginPath();
+            painter.MoveTo(tip);
+            painter.LineTo(a);
+            painter.LineTo(b);
+            painter.LineTo(tip);
+            painter.Fill(FillRule.OddEven);
+        }
+
+        private Vector2 OffsetToEdge(Vector2 localPos, Vector2 dir)
         {
             Vector2 viewPos = this.LocalToWorld(localPos);
             var node = GraphView.GetNodeAt(viewPos);
