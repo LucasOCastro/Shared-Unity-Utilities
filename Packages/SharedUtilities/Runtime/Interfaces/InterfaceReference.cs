7434e0bb44313abeb9b4fed1a05d9249346ca264
lucas castro <lucas_o__castro@hotmail.com>
2025-01-21 14:27:53 -0300

    WIP Interface Reference class

---
+++ Packages/SharedUtilities/Runtime/Interfaces/InterfaceReference.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using JetBrains.Annotations;
+using SharedUtilities.Extensions;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace SharedUtilities.Interfaces
+{
+    [Serializable]
+    public class InterfaceReference<TInterface, TObject> where TInterface : class where TObject : Object
+    {
+        [field: SerializeField] [CanBeNull]
+        public TObject UnderlyingValue { get; set; }
+
+        [CanBeNull]
+        public TInterface Value
+        {
+            get
+            {
+                return UnderlyingValue.OrNull() switch
+                {
+                    null => null,
+                    TInterface interfaceValue => interfaceValue,
+                    _ => throw new InvalidCastException($"Underlying value {UnderlyingValue} of type {typeof(TObject).Name} needs to implement {typeof(TInterface).Name}")
+                };
+            }
+
+            set => UnderlyingValue = value.OrNull() switch
+            {
+                null => null,
+                TObject objectValue => objectValue,
+                _ => throw new InvalidCastException($"Value {value} of type {typeof(TInterface).Name} needs to be of type {typeof(TObject).Name}")
+            };
+        }
+
+        public InterfaceReference()
+        {
+        }
+        
+        public InterfaceReference(TObject underlyingValue) => UnderlyingValue = underlyingValue;
+        
+        public InterfaceReference(TInterface interfaceValue) => Value = interfaceValue;
+    }
+    
+    [Serializable]
+    public class InterfaceReference<TInterface> : InterfaceReference<TInterface, Object> where TInterface : class { }
+}
