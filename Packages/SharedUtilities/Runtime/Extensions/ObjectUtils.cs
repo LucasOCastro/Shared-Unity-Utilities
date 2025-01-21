68d5cdbdf72165940b1dda66316e5d1b3df4b95c
lucas castro <lucas_o__castro@hotmail.com>
2025-01-21 14:27:26 -0300

    Fixes OrNull method

--- Packages/SharedUtilities/Runtime/Extensions/ObjectUtils.cs
+++ Packages/SharedUtilities/Runtime/Extensions/ObjectUtils.cs
@@ -1,12 +1,11 @@
 ﻿using JetBrains.Annotations;
-using UnityEngine;
 
 namespace SharedUtilities.Extensions
 {
     public static class ObjectUtils
     {
         [CanBeNull]
-        public static T OrNull<T>(this T obj) where T : Object
-            => obj ? obj : null;
+        public static T OrNull<T>(this T obj) where T : class
+            => obj.Equals(null) ? null : obj;
     }
 }
