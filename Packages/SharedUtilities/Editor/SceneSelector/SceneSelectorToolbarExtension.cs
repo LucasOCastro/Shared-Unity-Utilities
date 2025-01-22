2a421d7c195400d51c653c63a5e6c800f483b18a
lucas castro <lucas_o__castro@hotmail.com>
2025-01-22 11:21:03 -0300

    Scene Toolbar uses ScriptableSettings

---
+++ Packages/SharedUtilities/Editor/SceneSelector/SceneSelectorToolbarExtension.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SharedUtilities.Extensions;
+using Toolbox.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace SharedUtilities.Editor.SceneSelector
+{
+    [InitializeOnLoad]
+    public static class SceneSelectorToolbarExtension
+    {
+        static SceneSelectorToolbarExtension()
+        {
+            ToolboxEditorToolbar.OnToolbarGui += OnToolbarGui;
+        }
+
+        private static IEnumerable<string> GetSceneGuids()
+        {
+            var settings = SceneSelectorToolbarSettings.GetOrCreate();
+            return settings.ScenesToShow switch
+            {
+                SceneSelectorToolbarSettings.SceneListMode.ListedInBuildAndEnabled =>
+                    EditorBuildSettings.scenes
+                        .Where(s => s.enabled)
+                        .Select(s => s.guid.ToString()),
+                SceneSelectorToolbarSettings.SceneListMode.ListedInBuild =>
+                    EditorBuildSettings.scenes
+                        .Select(s => s.guid.ToString()),
+                SceneSelectorToolbarSettings.SceneListMode.AllInProject =>
+                    AssetDatabase.FindAssets("t:scene",
+                        settings.AllInProjectDirectories
+                            .WhereNotNullOrEmpty()
+                            .ToArray()
+                    ),
+                _ => Enumerable.Empty<string>()
+            };
+        }
+
+        private static void OnToolbarGui()
+        {
+            GUILayout.FlexibleSpace();
+            foreach (string guid in GetSceneGuids())
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(assetPath);
+                if (!sceneAsset) continue;
+                
+                if (GUILayout.Button(sceneAsset.name))
+                {
+                    EditorSceneUtility.StartScene(assetPath);
+                }
+            }
+        }
+    }
+}
---
+++ Packages/SharedUtilities/Editor/SceneSelector/SceneSelectorToolbarSettings.cs
@@ -0,0 +1,23 @@
+﻿using SharedUtilities.Settings;
+using UnityEngine;
+
+namespace SharedUtilities.Editor.SceneSelector
+{
+    [ScriptableSettings("Project/Scene Selector Toolbar")]
+    public class SceneSelectorToolbarSettings : ScriptableSettings
+    {
+        public enum SceneListMode
+        {
+            ListedInBuildAndEnabled = 0,
+            ListedInBuild = 1,
+            AllInProject = 2
+        }
+        
+        public SceneListMode ScenesToShow = SceneListMode.ListedInBuildAndEnabled;
+        
+        [ShowIf(nameof(ScenesToShow), SceneListMode.AllInProject)]
+        public string[] AllInProjectDirectories = { "Assets/_Project" };
+        
+        public static SceneSelectorToolbarSettings GetOrCreate() => GetOrCreate<SceneSelectorToolbarSettings>();
+    }
+}
