ebd00687506faac0358de8f25d41cf47b058b6cb
lucas castro <lucas_o__castro@hotmail.com>
2025-01-22 14:42:17 -0300

    SerializedPropertyUtils.cs to find serialized backing fields

---
+++ Packages/SharedUtilities/Editor/Extensions/SerializedObjectUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using SharedUtilities.Extensions;
+using UnityEditor;
+
+namespace SharedUtilities.Editor.Extensions
+{
+    public static class SerializedObjectUtils
+    {
+        public static SerializedProperty FindBackingFieldProperty(this SerializedObject obj, string propertyPath)
+        {
+            string propertyName = propertyPath.Split('.').Last();
+            string backingFieldName = FieldUtils.GetBackingFieldName(propertyName);
+            return obj.FindProperty(backingFieldName);
+        }
+    }
+}
---
+++ Packages/SharedUtilities/Editor/Extensions/SerializedPropertyUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using SharedUtilities.Extensions;
+using UnityEditor;
+
+namespace SharedUtilities.Editor.Extensions
+{
+    public static class SerializedPropertyUtils
+    {
+        public static SerializedProperty FindBackingFieldPropertyRelative(this SerializedProperty property,
+            string propertyPath)
+
+        {
+            string propertyName = propertyPath.Split('.').Last();
+            string backingFieldName = FieldUtils.GetBackingFieldName(propertyName);
+            return property.FindPropertyRelative(backingFieldName);
+        }
+    }
+}
