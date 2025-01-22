bd8ec4a1863fbaed660e1d18972e6172b94cd80c
lucas castro <lucas_o__castro@hotmail.com>
2025-01-22 11:57:09 -0300

    ListUtils for AddIfNotContains

---
+++ Packages/SharedUtilities/Runtime/Extensions/ListUtils.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SharedUtilities.Extensions
+{
+    public static class ListUtils
+    {
+        public static void AddIfNotContains<T>(this List<T> list, T item)
+        {
+            if (!list.Contains(item))
+                list.Add(item);
+        }
+    }
+}
