c19bfa7412f9890e4f0277b193a550ae92caf8c7
lucas castro <lucas_o__castro@hotmail.com>
2025-01-23 12:09:48 -0300

    RequiresInterfaceAttribute.cs as an alternative to InterfaceReference

---
+++ Packages/SharedUtilities/Editor/Interfaces/RequiresInterfaceAttributeDrawer.cs
@@ -0,0 +1,36 @@
+﻿using SharedUtilities.Interfaces;
+using UnityEditor;
+using UnityEngine;
+
+namespace SharedUtilities.Editor.Interfaces
+{
+    [CustomPropertyDrawer(typeof(RequiresInterfaceAttribute))]
+    public class RequiresInterfaceAttributeDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var attr = (RequiresInterfaceAttribute)attribute;
+            var interfaceType = attr.InterfaceType;
+            var objectType = fieldInfo.FieldType;
+            if (interfaceType == null)
+            {
+                Debug.LogError($"Could not get {nameof(RequiresInterfaceAttribute)} type arguments for {property.name}", property.serializedObject.targetObject);
+                EditorGUI.PropertyField(position, property, label);
+                return;
+            }
+            
+            if (!typeof(Object).IsAssignableFrom(objectType))
+            {
+                Debug.LogError($"{property.name} must be of type {nameof(Object)}", property.serializedObject.targetObject);
+                EditorGUI.PropertyField(position, property, label);
+                return;
+            }
+            
+            EditorGUI.BeginProperty(position, label, property);
+            
+            InterfaceReferenceUtils.DrawProperty(position, property, label, objectType, interfaceType);
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Packages/SharedUtilities/Runtime/Interfaces/RequiresInterfaceAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace SharedUtilities.Interfaces
+{
+    public class RequiresInterfaceAttribute : PropertyAttribute
+    {
+        public Type InterfaceType { get; }
+
+        public RequiresInterfaceAttribute(Type interfaceType)
+        {
+            InterfaceType = interfaceType;
+        }
+    }
+}
