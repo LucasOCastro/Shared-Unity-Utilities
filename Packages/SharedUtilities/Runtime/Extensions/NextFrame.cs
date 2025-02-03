00a03c8db6abc14bb0c4a4fe93328f4831b721a7
lucas castro <lucas_o__castro@hotmail.com>
2025-02-03 11:52:09 -0300

    NextFrame class

---
+++ Packages/SharedUtilities/Runtime/Extensions/NextFrame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine.Device;
+
+namespace SharedUtilities.Extensions
+{
+    public static class NextFrame
+    {
+        public static void Do(Action action, bool forceEditor = false)
+        {
+#if UNITY_EDITOR
+            if (!Application.isPlaying || forceEditor)
+            {
+                UnityEditor.EditorApplication.update += ActionThenUnregister;
+
+                return;
+
+                void ActionThenUnregister()
+                {
+                    action();
+                    UnityEditor.EditorApplication.update -= ActionThenUnregister;
+                }
+            }
+#endif
+
+            Task.Yield().AsTask().ContinueWith(action);
+        }
+    }
+}
