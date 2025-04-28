0fe4ec25ce42f813409a258e86ef4d1280820cee
lucas castro <lucas_o__castro@hotmail.com>
2025-04-28 08:31:32 -0300

    WIP progress on edge inspector

--- Packages/SharedStateMachines/Editor/EdgeInspector.cs
+++ Packages/SharedStateMachines/Editor/EdgeInspector.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using SharedUtilities.Extensions;
 using SharedUtilities.StateMachines.Editor.Graph;
+using SharedUtilities.VisualElements;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Object = UnityEngine.Object;
@@ -13,32 +15,30 @@ namespace SharedUtilities.StateMachines.Editor
     public class EdgeInspector : IInspector
     {
         public ArrowEdge Edge { get; }
-        private readonly Dictionary<TransitionAsset, UnityEditor.Editor> _editors;
         
         public EdgeInspector(ArrowEdge edge)
         {
             Edge = edge;
-            _editors = edge.Assets.ToDictionary(t => t, UnityEditor.Editor.CreateEditor);
         }
         
         public void CreateGUI(VisualElement container)
         {
-            container.Add(new Label { text = Edge.Assets.Aggregate("", (a, b) => a + b.name) });
-            
             ListView list = new(Edge.Assets)
             {
                 showAddRemoveFooter = true,
                 reorderable = true,
-                reorderMode = ListViewReorderMode.Animated,
-                showFoldoutHeader = true,
-                headerTitle = "TEST",
+                reorderMode = ListViewReorderMode.Simple,
+                headerTitle = "Transitions",
                 virtualizationMethod = CollectionVirtualizationMethod.DynamicHeight,
                 makeItem = MakeItem,
                 bindItem = BindItem,
                 unbindItem = UnbindItem,
-                onAdd = OnAdd,
                 overridingAddButtonBehavior = OverridingAddButtonBehavior
             };
+            
+            list.itemsAdded += OnItemsAdded;
+            list.itemsRemoved += OnItemsRemoved;
+            
             container.Add(list);
         }
         
@@ -50,21 +50,52 @@ namespace SharedUtilities.StateMachines.Editor
         private void BindItem(VisualElement item, int index)
         {
             var asset = Edge.Assets[index];
-            var editor = _editors[asset];
-            item.Add(editor.CreateInspectorGUI());
+            var subElements = GetFieldSubElements(asset.transition, nameof(asset.transition), asset).ToList();
+            
+            // TODO make label and foldout text editable
+            if (subElements.Count == 0)
+            {
+                //item.Add(new Label(asset.name));
+                var label = new EditableLabel(asset.name);
+                label.ValueChanged += (ref string name) =>
+                {
+                    if (string.IsNullOrWhiteSpace(name))
+                        name = GenerateNameForTransition(asset);
+                    asset.name = name;
+                };
+                item.Add(label);
+                return;
+            }
+            
+            var foldout = new Foldout
+            {
+                text = asset.name, 
+                value = false
+            };
+            subElements.ForEach(foldout.Add);
+            item.Add(foldout);
         }
         
-        private void UnbindItem(VisualElement item, int index)
-        {
+        private static void UnbindItem(VisualElement item, int index)
+        {            
             item.Clear();
         }
         
-        private void OnAdd(BaseListView obj)
+        private static void OnItemsAdded(IEnumerable<int> indices)
         {
-            Debug.Log("On add " + obj);
-            
-            //Edge.GraphView.Asset.transitions.Add();
+            Debug.Log("On add " + indices.ToCommaSeparatedString());
         }
+
+        private void OnItemsRemoved(IEnumerable<int> indices)
+        {
+            foreach (int index in indices.OrderByDescending(i => i))
+            {
+                var asset = Edge.Assets[index];
+                Edge.GraphView.Asset.RemoveAndDestroy(asset);
+            }
+        }
+        
+        
         
         private void OverridingAddButtonBehavior(BaseListView list, Button button)
         {
@@ -79,12 +110,12 @@ namespace SharedUtilities.StateMachines.Editor
         private void OnTypeSelected(Type type)
         {
             var asset = ScriptableObject.CreateInstance<TransitionAsset>();
-            asset.name = $"{type.GetDisplayName()} ({Edge.OutputState?.name} -> {Edge.InputState?.name})";
             asset.from = Edge.OutputState;
             asset.to = Edge.InputState;
             asset.transitionType = type.AssemblyQualifiedName;
+            asset.name = GenerateNameForTransition(asset);
             
-            asset.transition = (IBaseTransition)TypeUtils.InstantiateObject(type);
+            asset.transition = (IBaseTransition)ObjectUtils.InstantiateObject(type);
             var state = asset.to.OrNull()?.state;
             asset.transition.To = state;
             
@@ -93,7 +124,6 @@ namespace SharedUtilities.StateMachines.Editor
         
         public void Dispose()
         {
-            _editors.Values.ForEach(Object.DestroyImmediate);
         }
 
         private Type[] GetTransitionTypes(out GUIContent[] options)
@@ -108,6 +138,32 @@ namespace SharedUtilities.StateMachines.Editor
                 options[i] = new(types[i].GetDisplayName());
 
             return types;
+        }
+
+        private static IEnumerable<VisualElement> GetFieldSubElements(object obj, string name, Object parent)
+        {
+            if (obj is Object o)
+            {
+                yield return new InspectorElement(o);
+                yield break;
+            }
+            
+            var so = new SerializedObject(parent);
+            var prop = so.FindProperty(name);
+            while (prop.NextVisible(true))
+            {
+                var field = new PropertyField(prop.Copy());
+                field.Bind(so);
+                yield return field;
+            }
+        }
+        
+        private static string GenerateNameForTransition(TransitionAsset asset)
+        {
+            var from = asset.from;
+            var to = asset.to;
+            var transitionType = Type.GetType(asset.transitionType).GetDisplayName();
+            return $"{transitionType} ({from.name} -> {to.name})";
         }
     }
 }
