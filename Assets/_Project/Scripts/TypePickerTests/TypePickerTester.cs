1e98b963a8d359a5896e9e9ee6fa41200d6e1f5d
lucas castro <lucas_o__castro@hotmail.com>
2025-05-05 10:57:57 -0300

    Test for the type picker attribute

---
+++ Assets/_Project/Scripts/TypePickerTests/TypePickerTester.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SharedUtilities.Serialization;
+using UnityEngine;
+
+namespace SharedUtilitiesPackages.TypePickerTests
+{
+    [Serializable]
+    public class BaseClass
+    {
+        public int BaseClassA;
+    }
+
+    [Serializable]
+    public class DerivedClass : BaseClass
+    {
+        public int DerivedClassA;
+    }
+
+    [Serializable]
+    public abstract class AbstractDerivedClass : BaseClass
+    {
+        public int AbstractDerivedClassA;
+    }
+    
+    [Serializable]
+    public class AbstractDerivedClass2 : AbstractDerivedClass
+    {
+        public int AbstractDerivedClass2A;
+    }
+    
+    public class TypePickerTester : MonoBehaviour
+    {
+        [SerializeReference, TypePicker] public BaseClass AnyTypeBasically;
+        [SerializeReference, TypePicker] public AbstractDerivedClass FromAbstract;
+        [SerializeReference, TypePicker(typeof(AbstractDerivedClass2))] public BaseClass Limited;
+    }
+}
